b1ef9286509230eabfa08752036c6c4022310a27
Luciano Marisi <luciano@techbrewers.com>
2016-02-14 16:07:36 +0000

    Added tests for function dispatching

--- DispatcherTests/DispatcherTests.swift
+++ DispatcherTests/DispatcherTests.swift
@@ -13,8 +13,8 @@ class DispatcherTests: XCTestCase {
 
   let acceptableTolerance = 0.2 // seconds, test suite appears to run slower than app
   
-  func testMockPoints() {
-    let expectation = expectationWithDescription("MockPoints")
+  func testMockPointsDispatching() {
+    let expectation = expectationWithDescription("testMockPointsDispatching")
     let dispatcher = Dispatcher()
     var mockPoints = [Point]()
 
@@ -42,4 +42,42 @@ class DispatcherTests: XCTestCase {
   
   }
   
+  func testFunctionDispatching() {
+    let expectation = expectationWithDescription("testFunctionDispatching")
+    let dispatcher = Dispatcher()
+    let startDate = NSDate()
+    let totalTime = 1.0
+    dispatcher.startWithFunction(sineSignal) { (point) -> Void in
+      NSLog("\(point.value)")
+      let expectedValue = self.sineSignal(point.timestamp).value
+      XCTAssertEqual(expectedValue, point.value)
+      let currentDate = NSDate()
+      let difference = currentDate.timeIntervalSinceDate(startDate)
+      let deviation = difference - point.timestamp
+      XCTAssert(abs(deviation) < self.acceptableTolerance)
+    }
+    
+    dispatcher.scheduleCompleteClosure = {
+      expectation.fulfill()
+    }
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(totalTime * Double(NSEC_PER_SEC))), dispatch_get_main_queue()) {
+      dispatcher.stop()
+    }
+    waitForExpectationsWithTimeout(totalTime + 1) { error in XCTAssert(error == nil) }
+    
+  }
+  
+  // MARK: Helpers
+  
+  func sineSignal(nextTimestamp: NSTimeInterval) -> Point {
+    let signalFrequency = 1.0
+    let amplitude = 2.0
+    let offset = 0.5
+    let phaseShift = 0.2
+    let value = amplitude * sin(nextTimestamp * signalFrequency + phaseShift) + offset
+    return Point(timestamp: nextTimestamp, value: value)
+  }
+  
 }
+
